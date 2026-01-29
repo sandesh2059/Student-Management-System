9beea0f1e0f2319cfee3c0310a9310fbceb3eb7c
Sandesh <sandeshchy2059@gmail.com>
2026-01-29 22:03:50 +0545

    model created for student

--- Student_management/students/models.py
+++ Student_management/students/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Student(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    grade = models.CharField(max_length = 10)
+    courses = models.ManyToManyField(Course, many = True )
+
+    def __str__(self):
+        return self.user.username
