7085b2073f49b2ea4827d67bece7135e15a06b93
Sandesh <sandeshchy2059@gmail.com>
2026-02-04 21:28:45 +0545

    class coursedetailAPI created

--- Student_management/students/views.py
+++ Student_management/students/views.py
@@ -1,4 +1,5 @@
 from rest_framework.response import Response
+from rest_framework import status
 from rest_framework.views import APIView
 from .models import Course, Student, Teacher
 from .serializers import CourseSerializer, StudentSerializer, TeacherSerializer
@@ -15,4 +16,37 @@ class CourseCreateAPI(APIView):
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
+    
+class CourseDetailAPI(APIView):
+    def get_object(self, request, pk):
+        try:
+            course = Course.objects.get(pk=pk)
+        except Course.DoesNotExist:
+            return Response('error, course not found')
+        
+    def get(self, request, pk):
+        course = self.get_object(pk)
+        if not course:
+            return Response({'error': 'Course not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
+    
+    def put(self, request, pk):
+        course = self.get_object(pk)
+        if not course:
+            return Response({'error': 'Course not found'}, status=status.HTTP_404_NOT_FOUND)
+        serializer = CourseSerializer(course, data = request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+    
+    def delete(self, request, pk):
+        course = self.get_object(pk)
+        if not course:
+            return Response({'error': 'Course not found'}, status=status.HTTP_404_NOT_FOUND)
+        course.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+        
+
     
